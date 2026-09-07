a239eda8d118047aae0f2ca7e68005a233feaf61
annrosabinu <76247572+annrosabinu@users.noreply.github.com>
2026-09-07 12:31:35 +0530

    Update views.py

--- ajaxproject/ajaxapp/views.py
+++ ajaxproject/ajaxapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Student
 from django.http import JsonResponse
-# Create your views here.
+# Create  views 
 def home(request):
     return render(request,'home.html')
 def add_item(request):
